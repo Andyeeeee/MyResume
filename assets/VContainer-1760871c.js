d7a3ed78020ee69b24b7747d4656b41cebe7c664
Andyeeeee <127477711+Andyeeeee@users.noreply.github.com>
2024-01-31 23:49:05 +0000

    Deploying to gh-pages from @ Andyeeeee/MyResumeUnfinished-main2.0@4ce1b3b78682cab630a7aec69308c810d58352d6 🚀

--- assets/VContainer-1760871c.js
+++
@@ -1,1 +0,0 @@
-import{p as n,m as o,j as r,k as l,u as i,l as c,b as u}from"./index-901a3544.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),C=l()({name:"VContainer",props:m(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=i();return c(()=>u(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{C as V};
