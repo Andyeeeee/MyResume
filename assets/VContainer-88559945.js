a5d7447c56726fd69bd8f8a28b37088fb83dc30d
Andyeeeee <127477711+Andyeeeee@users.noreply.github.com>
2024-02-04 05:08:56 +0000

    Deploying to gh-pages from @ Andyeeeee/MyResume@83b0c803ba3eb5439c3c1502a149d5212c0a23ab 🚀

--- assets/VContainer-88559945.js
+++
@@ -1,1 +0,0 @@
-import{p as n,m as o,k as r,l,u as i,q as c,b as u}from"./index-d886e64b.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),C=l()({name:"VContainer",props:m(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=i();return c(()=>u(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{C as V};
